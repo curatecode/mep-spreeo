11c4c4a2214d96af12f41829c1eff54b5c744e7e
Wawan <syzwnzhr@gmail.com>
2017-10-19 18:38:09 +0800

    added bootstrap and js

--- gulpfile.js
+++ gulpfile.js
@@ -1,19 +1,23 @@
 var gulp        = require('gulp'),
 		pug         = require('gulp-pug'),
 		sass        = require('gulp-sass'),
+		concat      = require('gulp-concat'),
 		uglify      = require('gulp-uglify'),
 		browserSync = require('browser-sync').create();
 
 gulp.task('serve', ['sass'], function() {
   browserSync.init({
-      server: "./public"
+      server: "./public",
+      open: false
   });
   gulp.watch("source/assets/scss/*.scss", ['sass']);
   gulp.watch("source/**/*.pug", ["views"]).on('change', browserSync.reload);
+  gulp.watch("source/assets/js/*.js", ["js-watch"]);
+  gulp.watch("source/vendor/**/*", ["copy"]);
 });
 
 gulp.task('views', function buildHTML() {
-	return gulp.src('source/**/*.pug')
+	return gulp.src('source/**/!(_)*.pug')
 		.pipe(pug({
 			pretty: true
 		}))
@@ -23,15 +27,20 @@ gulp.task('views', function buildHTML() {
 gulp.task('sass', function() {
   return gulp.src("source/assets/scss/*.scss")
 	  .pipe(sass())
+	  .pipe(concat('main.css'))
 	  .pipe(gulp.dest("public/assets/css"))
 	  .pipe(browserSync.stream());
 });
 
 gulp.task('js-watch', function() {
   return gulp.src("source/assets/js/*.js")
-	  .pipe(browserify())
-	  .pipe(uglify())
+	  // .pipe(uglify())
 	  .pipe(gulp.dest("public/assets/js"));
+});
+
+gulp.task('copy', function() {
+	return gulp.src(['source/vendor/**/*'])
+		.pipe(gulp.dest('public/assets'))
 });
 
 gulp.task('default', ['serve']);
